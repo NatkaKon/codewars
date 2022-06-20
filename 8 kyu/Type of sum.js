7ff23142751935a6ce2ed7fb0c9fb4e716af550c
Natalia Kondratieva <kondratieva07@gmail.com>
2022-06-20 20:39:13 +0300

    feat: task done

---
+++ 8 kyu/Type of sum.js
@@ -0,0 +1,9 @@
+//Return the type of the sum of the two arguments
+
+function typeOfSum(a, b) {
+    let summ = a + b;
+    return typeof(summ)
+  }
+
+
+//const typeOfSum = (a, b) => typeof(a + b);
